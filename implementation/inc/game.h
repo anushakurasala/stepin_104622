e7fffcd4f5508812cd6f666732f653a13e0eecae
anushakurasala <69642245+anushakurasala@users.noreply.github.com>
2020-11-14 14:26:47 +0530

    first commit

---
+++ implementation/inc/game.h
@@ -0,0 +1,14 @@
+/**
+* @file game.h
+*
+*/
+#ifndef __GAME_H__
+#define __GAME_H__
+
+
+int generateRandomNumber(int number);
+int greater(char c1, char c2);
+void help();
+void exit();
+
+#endif /* #ifndef __GAME_H__ */
